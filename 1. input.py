9e5178413a22e816e8f3a2e544ed817e00b760cd
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-05 23:53:13 +0530

    update

--- 1. input.py
+++ 1. input.py
@@ -1,10 +1,20 @@
 '''
+This reference files is created by Kalpit Rathod.
+'''
+
+'''
 variable will store the values input by the user
 functions will run predefined task
 arguments is the input for the functions
 when we call the variable in print it will return value
 Bugs is the mistake in programming.
-
 '''
+# this is comment 
+
+# Taking the input from the input function and store it in the name variable 
 name = input("Enter your name: ")
-print("Hello, Kalpit")
+# Now this will print the hard text
+print("Hello,")
+# This will print the variable stored value 
+print(name)
+
