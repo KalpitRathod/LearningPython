85f6775e77167d2aedd8819ddb2d8ad71411fd4e
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-06 00:13:19 +0530

    hola 2

--- 1. input.py
+++ 1. input.py
@@ -3,18 +3,25 @@ This reference files is created by Kalpit Rathod.
 '''
 
 '''
-variable will store the values input by the user
-functions will run predefined task
-arguments is the input for the functions
-when we call the variable in print it will return value
+Variable will store the values input by the user
+Functions will run predefined task
+Arguments is the input for the functions
+When we call the variable in print it will return value
 Bugs is the mistake in programming.
 '''
-# this is comment 
+# this is comment
 
-# Taking the input from the input function and store it in the name variable 
+# Taking the input from the input function and store it in the name variable
 name = input("Enter your name: ")
 # Now this will print the hard text
-print("Hello,")
-# This will print the variable stored value 
-print(name)
+print("Hello, "+name) # This will use only one argument
+print("Hello,",name) # This will use two arguments
+# This will print the variable stored value
+print(name, 'This is "Comma"')
+print(name, "This is \"Comma\"")
+print(name, f"This is {name}") #format string
 
+# string is the array of charactors or this is the type of data in python it known as the str
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+# sep is seperator, when we pass multiple arguments it will separate with the dafault space
+# end is the \n means new line, dafault is the always new line
