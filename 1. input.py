858d59bbcaf3e66d8c2ae59474957eb8c5372f67
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-05 23:40:13 +0530

    first commit

---
+++ 1. input.py
@@ -0,0 +1,10 @@
+'''
+variable will store the values input by the user
+functions will run predefined task
+arguments is the input for the functions
+when we call the variable in print it will return value
+Bugs is the mistake in programming.
+
+'''
+name = input("Enter your name: ")
+print("Hello, Kalpit")
