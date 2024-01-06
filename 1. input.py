0ec0f37f539b84925ae15aa82888ae6740c703dc
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-06 22:39:48 +0530

    new file

--- 1. input.py
+++ 1. input.py
@@ -12,16 +12,38 @@ Bugs is the mistake in programming.
 # this is comment
 
 # Taking the input from the input function and store it in the name variable
+'''
 name = input("Enter your name: ")
+'''
 # Now this will print the hard text
+'''
 print("Hello, "+name) # This will use only one argument
 print("Hello,",name) # This will use two arguments
+'''
 # This will print the variable stored value
+'''
 print(name, 'This is "Comma"')
 print(name, "This is \"Comma\"")
 print(name, f"This is {name}") #format string
+'''
 
 # string is the array of charactors or this is the type of data in python it known as the str
 # print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 # sep is seperator, when we pass multiple arguments it will separate with the dafault space
 # end is the \n means new line, dafault is the always new line
+
+'''
+name = input("Name: ").strip().title()
+# Remove whitespace from str
+# name = name.strip()
+# name = name.capitalize() #capitalized first letter
+# name = name.title() #capitalized first letter each word
+print(f"This is {name}") #format string
+'''
+
+'''
+# Split use name into first name and last name 
+name = input("Name: ").strip().title()
+first, last = name.split(" ")
+print("First Name: "+first+" And Last Name: "+last)
+'''
---
+++ 2. cal.py
@@ -0,0 +1,31 @@
+# Calculator
+'''
+x = 1
+y = 1
+
+z = x+y
+
+print(z)
+'''
+
+'''
+we can debug if user inputed other than integer
+x = input("What's x? ")
+y = input("What's y? ")
+
+z = int(x) + int(y) #input will take all data in string and we need to convert it to the number
+
+print(z)
+'''
+
+'''
+# expect what will we have 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+print(x + y)
+'''
+
+# one line code 
+# print(int(input("What's x? ")) + int(input("What's y? ")))
+
