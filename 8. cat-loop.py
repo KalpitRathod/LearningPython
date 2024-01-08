5d6508e41f68f065cd6fa37b3fd642493764760d
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-08 17:04:57 +0530

    2 new files

---
+++ 8. cat-loop.py
@@ -0,0 +1,62 @@
+# this is while loop 
+# i = 3
+# while i !=0:
+#     print("meow") 
+#     i = i - 1
+    
+# i = 0
+# while i != 3:
+#     print("meow") 
+#     i = i + 1
+
+# i = 1
+# while i <= 3:
+#     print("meow") 
+#     i = i + 1
+    
+# i = 0
+# while i < 3:
+#     print("meow") 
+#     i += 1
+
+# for i in ["b","a","c"]: #whatever things i put in those 3 places it is running 3 times str, number or bool.\
+#     print("meow")
+    
+# for i in range(3):
+#     print("meow")
+
+# for _ in range(3):
+#     print("meow")
+#     print(_)
+
+# print("meow\n" * 3, end="")
+
+'''
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+    
+for _ in range(n):
+    print("meow")
+'''
+
+'''
+def main():
+    number = get_number()
+    meow(number)
+    
+def get_number():
+    while True:
+        n = int(input("What's num? "))
+        if n > 0:
+            break # we can direct return n here instead break
+    return n
+    
+def meow(n):
+    for _ in range(n):
+        print("meow")
+        
+main()
+'''
+
