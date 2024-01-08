07bdad9e2d13605c93b20657fcbd3469948635ab
KalpitRathod <99kalpitrathod@gmail.com>
2024-01-08 19:06:01 +0530

    new filrs on list, dict, loops

---
+++ 9. list-hogwards.py
@@ -0,0 +1,11 @@
+students = ["Hermione", "Harry", "Ron"]
+
+# print(students)
+
+# print(students[1])
+
+# for student in students:
+#     print(student)
+
+# for i in range(len(students)): 
+#     print(i, students[i]) 
